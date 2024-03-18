d0f8de7c6b5112cee311a6dda2bf1ec66b779eab
Isabelliaiui <isabellaestuda90750@gmail.com>
2024-03-18 09:56:00 -0300

    formulário

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar);
 var nome = document.getElementById("nome");
 
+var saida = document.getElementById("saida-de-dados");
+
+var email = document.getElementById("email");
+
 function alertar(){
-    alert(nome.value + " " + "Você clicou no botão!!!")
+    //alert(nome.value + " " + "clicou no botão!!!");
+    saida.innerText = "Nome: " + " " + nome.value +
+    "\n E-mail: " + " " + email.value +
+    "\n Telefone: " + " " + Telefone +
+    "\n CEP: " + " " + CEP +
+    "\n logradouro: " + " " +  logradouro +
+    "\n número: " + " " + número +
+    "\n complemento: " + " " + complemento +
+    "\n bairro: " + " " + bairro +
+    "\n cidade: " + " " +  cidade +
+    "\n estado: " + " " + estado;
 }
