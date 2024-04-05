4eab0a3060582fc0a1312fabefaf320003d4ad3e
Isabelliaiui <isabellaestuda90750@gmail.com>
2024-04-05 08:25:37 -0300

    Formulario atualizado

--- script.js
+++ script.js
@@ -6,16 +6,49 @@ var saida = document.getElementById("saida-de-dados");
 
 var email = document.getElementById("email");
 
+var Telefone = document.getElementById("Telefone");
+
+var CEP = document.getElementById("CEP");
+
+var logradouro = document.getElementById("logradouro");
+
+var número = document.getElementById("número");
+
+var complemento = document.getElementById("complemento");
+
+var bairro = document.getElementById("bairro");
+
+var cidade = document.getElementById("cidade");
+
+var estado = document.getElementById("estado");
+
 function alertar(){
+
+        //buscar o endereço pelo cep
+        const url =`https://viacep.com.br/ws/${cep.value}/json`;
+
+        fetch(url)
+        .then(resposta=>resposta.json())
+        .then(data => {
+            logradouro.value = data.logradouro;
+            bairro.value = data.bairro;
+            cidade.value = data.cidade;
+            bairro.value = data.bairro;
+            bairro.value = data.bairro;
+
+            saida.innerText = "Nome: " + " " + nome.value +
+            "\n E-mail: " + " " + email.value + nome.value +
+            "\n Telefone: " + " " + Telefone + nome.value +
+            "\n CEP: " + " " + CEP + nome.value + 
+            "\n logradouro: " + " " +  logradouro + nome.value +
+            "\n número: " + " " + número + nome.value +
+            "\n complemento: " + " " + complemento + nome.value +
+            "\n bairro: " + " " + bairro + nome.value +
+            "\n cidade: " + " " +  cidade + nome.value +
+            "\n estado: " + " " + estado + nome.value;
+        })
+        .catch(error=>alert(error))
+
     //alert(nome.value + " " + "clicou no botão!!!");
-    saida.innerText = "Nome: " + " " + nome.value +
-    "\n E-mail: " + " " + email.value +
-    "\n Telefone: " + " " + Telefone +
-    "\n CEP: " + " " + CEP +
-    "\n logradouro: " + " " +  logradouro +
-    "\n número: " + " " + número +
-    "\n complemento: " + " " + complemento +
-    "\n bairro: " + " " + bairro +
-    "\n cidade: " + " " +  cidade +
-    "\n estado: " + " " + estado;
+
 }
